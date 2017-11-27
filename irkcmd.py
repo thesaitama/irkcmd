1ee73c9938bb6953f9be47b83ea701d07a84eaa2
Kazuhiro Komiya <kazuhiro@altar.jp>
2017-11-27 22:57:23 +0900

    fix - coding style

--- irkcmd.py
+++ irkcmd.py
@@ -4,19 +4,23 @@
 # irkcmd.py
 
 """
-ローカルで IRKit をプリセットコントロールするためのスクリプト
+IR Kit CLI Script (local network only)
 """
 
-import os, sys, json
+import os
+import sys
+import json
 import requests
 
-__version__ = '0.0.1'
+g_cmd_data = {}
+
+__version__ = '0.0.2.171127'
 
 def irkMain():
     '''
     メインルーチン
     '''
-    global g_cmdData # 設定用データ
+    global g_cmd_data # 設定用データ
 
     argv = sys.argv
     argc = len(argv)
@@ -28,8 +32,8 @@ def irkMain():
     cmd = str(argv[1])
 
     # 設定ファイルの読み込み
-    scriptPath = os.path.dirname(os.path.abspath(__file__))
-    g_cmdData = loadConfig(scriptPath + '/irkconfig.json')
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    g_cmd_data = loadConfig(os.path.join(script_path, 'irkconfig.json'))
 
     if (cmd == 'cmdlist'):
         showCmds()
@@ -41,36 +45,35 @@ def showCmds():
     コマンドの一覧を表示する
     '''
 
-    cmdData = g_cmdData
-    
-    print '--cmdlist--'    
+    cmd_data = g_cmd_data
 
-    for cmd in sorted(cmdData):
-        print ('{0:20}: {1:30}'.format(cmd, cmdData[cmd]['memo']))
+    print '--cmdlist--'
+
+    for cmd in sorted(cmd_data):
+        print ('{0:20}: {1:30}'.format(cmd, cmd_data[cmd]['memo']))
         #print cmd + ': ' + cmdData[cmd]['memo']
 
 def execCmdSend(cmd):
     '''
     IRKit にデータを送信する
     '''
-    
-    cmdData = g_cmdData
+    cmd_data = g_cmd_data
 
     # 設定項目の存在を確認する
-    if(cmd in cmdData):
+    if(cmd in cmd_data):
 
-        target = cmdData[cmd]['target']
+        target = cmd_data[cmd]['target']
         headers = {'X-Requested-With': 'curl'}
-        format = cmdData[cmd]['format']
-        freq = cmdData[cmd]['freq']
-        data = cmdData[cmd]['data']
-        message = {'format': format, 'freq': freq, 'data': data}
-        reqUrl = 'http://' + target + '/messages'
-        
+        send_format = cmd_data[cmd]['format']
+        freq = cmd_data[cmd]['freq']
+        data = cmd_data[cmd]['data']
+        message = {'format': send_format, 'freq': freq, 'data': data}
+        reqUrl = 'http://%s/messages' % (target)
+
         # リクエストの送信
         r = requests.post(reqUrl, headers=headers, data=json.dumps(message))
         print r.status_code
-    
+
     # 設定項目がない場合
     else:
         print 'cmd not found'
@@ -80,15 +83,13 @@ def loadConfig(configFile):
     '''
     設定ファイルを読み出す
     '''
-
     if (os.path.exists(configFile)):
         f = open(configFile, 'r')
-        jsonData = json.load(f)
-        #print json.dumps(jsonData, sort_keys = True, indent = 4)
+        json_data = json.load(f)
+        #print json.dumps(json_data, sort_keys = True, indent = 4)
         f.close()
-        return jsonData
-    else:
-        return False
+        return json_data
+    return False
 
 if __name__ == '__main__':
     irkMain()
